fad4c34f39ebf9cd826004bb6f8e89e908d119aa
zzzhenng <cjlkindle01@gmail.com>
2018-02-23 16:07:52 +0800

    creating header Order Inventory

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+// import ReactDOM from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+render(<App/>, document.querySelector('#main'));
+
 
