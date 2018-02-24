44db5cf9f30f4ba4599a54e5c752b0cd1b61c401
zzzhenng <cjlkindle01@gmail.com>
2018-02-24 13:03:08 +0800

    add router

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" required/>
+        <input type="text" placeholder="Store Name" required 
+          defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import ReactDOM from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}></Match>
+        <Match pattern="/store/:storeId" component={App}></Match>
+        <Miss component={NotFound}></Miss>
+      </div>
+    </BrowserRouter>
+  )
+}
+render(<Root/>, document.querySelector('#main'));
 
 
